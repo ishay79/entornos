f826014a8b2425149cdc37af6c3db438005fcb26
DAVID <DAVID@DAVID-PC>
2014-04-28 17:37:44 +0200

    Añadimos la nueva clase

---
+++ src/paquetedavid/ClaseDavid.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paquetedavid;
+
+/**
+ *
+ * @author DAVID
+ */
+public class ClaseDavid {
+    public static void ClaseDavid(String [] args){
+        
+        System.out.println("Hola Isai");
+        System.out.println("Adios Isai");
+    }
+}
